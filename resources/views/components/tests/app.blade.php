a07d511dae2248bd87ec9ba1230d0c292929626e
naocraft0111 <naoki1306@gmail.com>
2022-08-14 14:31:35 +0900

    名前付きスロット

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -15,6 +15,9 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body>
+        <header>
+            {{ $header }}
+        </header>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
