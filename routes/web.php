7419e4bb1a3cc7d42485855e4da2fbef702f14b9
naocraft0111 <naoki1306@gmail.com>
2022-08-25 18:02:53 +0900

    cart delete

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::prefix('cart')->
     Route::get('/', [CartController::class, 'index'])->name('cart.index');
     Route::post('add', [CartController::class, 'add'])
                 ->name('cart.add');
+    Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
 });
 
 // Route::get('/dashboard', function () {
